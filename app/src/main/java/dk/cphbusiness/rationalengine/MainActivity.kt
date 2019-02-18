042425e52a580fec3cf8dda31fdea0369be26bc2
Anders Kalhauge <anders@kalhauge.dk>
2019-02-18 10:29:03 +0100

    lidt mere

--- app/src/main/java/dk/cphbusiness/rationalengine/MainActivity.kt
+++ app/src/main/java/dk/cphbusiness/rationalengine/MainActivity.kt
@@ -3,6 +3,7 @@ package dk.cphbusiness.rationalengine
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
@@ -10,16 +11,18 @@ import org.jetbrains.anko.toast
 //import android.util.log
 
 class MainActivity : AppCompatActivity() {
+    val LOG_KEY = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button.setOnClickListener {
-            toast("Button clicked")
+            Log.d(LOG_KEY, "Button clicked")
+
 //            val intent = Intent(this, OptionsActivity::class.java)
 //            startActivity(intent)
 
-            startActivity<OptionsActivity>()
+            startActivity<OptionsActivity>("name" to "Kurt")
             }
         }
     }
--- app/src/main/java/dk/cphbusiness/rationalengine/OptionsActivity.kt
+++ app/src/main/java/dk/cphbusiness/rationalengine/OptionsActivity.kt
@@ -2,11 +2,14 @@ package dk.cphbusiness.rationalengine
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_options.*
 
 class OptionsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
+        val name = intent.extras.getString("name")
+        options_label.setText("Options called by $name")
     }
 }
